9f5ccffa42585f0b59973935ece9afe3b861ca66
Kariyeva <karievakarina2006@gmail.com>
2026-05-24 01:59:17 +0500

    firebase env config

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
